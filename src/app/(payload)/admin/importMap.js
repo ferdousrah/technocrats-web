14a5c91fb80b33e93ac319731c68646060f7b675
Ferdus Rahman <ferdous.rahman.tapon@gmail.com>
2025-11-17 13:59:55 +0600

    Update importMap.js

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,3 +1,6 @@
+import { default as default_7834229b228a266ca510a3aaba96fcbf } from '../../../components/admin/collections/UsersList'
+import { default as default_c0f5f62cdd3d5d84754dc51bc7a02e96 } from '../../../components/admin/collections/MediaList'
+import { ServiceTypesList as ServiceTypesList_8f1d439cc289adf81730f9ebcc76c419 } from '../../../components/admin/collections/TaxonomyList'
 import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
 import { RscEntryLexicalField as RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
 import { LexicalDiffComponent as LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
@@ -21,11 +24,24 @@ import { StrikethroughFeatureClient as StrikethroughFeatureClient_e70f5e05f09f93
 import { UnderlineFeatureClient as UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { BoldFeatureClient as BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_9881d71b506a84b25a111185e995697a } from '../../../components/admin/collections/ServicesList'
+import { ProductCategoriesList as ProductCategoriesList_8f1d439cc289adf81730f9ebcc76c419 } from '../../../components/admin/collections/TaxonomyList'
+import { default as default_232c71002d77b6b56d2a30e2a1313b6f } from '../../../components/admin/collections/ProductsList'
+import { default as default_6e9af9d0749d2a65f8e2ddac574e1f33 } from '../../../components/admin/collections/ProjectsList'
+import { default as default_243b54c4a8a492dbddf073c234b55c27 } from '../../../components/admin/collections/TeamMembersList'
+import { default as default_1243ec043af9097e4440375e29baa310 } from '../../../components/admin/collections/TestimonialsList'
+import { BlogCategoriesList as BlogCategoriesList_8f1d439cc289adf81730f9ebcc76c419 } from '../../../components/admin/collections/TaxonomyList'
+import { BlogTagsList as BlogTagsList_8f1d439cc289adf81730f9ebcc76c419 } from '../../../components/admin/collections/TaxonomyList'
 import { BlocksFeatureClient as BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_61264ab934251a15e8ca2cc8f66bc206 } from '../../../components/admin/collections/BlogList'
+import { default as default_c23758562428c44820702092add4b596 } from '../../../components/admin/collections/ContactInquiriesList'
 import { default as default_f1a01e3ffbf6b019e40d01928db2823f } from '../../../components/admin/dashboard/CustomDashboard'
 import { default as default_9d80edae6c0796073a05eaf3a6882039 } from '../../../components/admin/views/AnalyticsView'
 
 export const importMap = {
+  "./components/admin/collections/UsersList#default": default_7834229b228a266ca510a3aaba96fcbf,
+  "./components/admin/collections/MediaList#default": default_c0f5f62cdd3d5d84754dc51bc7a02e96,
+  "./components/admin/collections/TaxonomyList#ServiceTypesList": ServiceTypesList_8f1d439cc289adf81730f9ebcc76c419,
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
   "@payloadcms/richtext-lexical/rsc#LexicalDiffComponent": LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e,
@@ -49,7 +65,17 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "./components/admin/collections/ServicesList#default": default_9881d71b506a84b25a111185e995697a,
+  "./components/admin/collections/TaxonomyList#ProductCategoriesList": ProductCategoriesList_8f1d439cc289adf81730f9ebcc76c419,
+  "./components/admin/collections/ProductsList#default": default_232c71002d77b6b56d2a30e2a1313b6f,
+  "./components/admin/collections/ProjectsList#default": default_6e9af9d0749d2a65f8e2ddac574e1f33,
+  "./components/admin/collections/TeamMembersList#default": default_243b54c4a8a492dbddf073c234b55c27,
+  "./components/admin/collections/TestimonialsList#default": default_1243ec043af9097e4440375e29baa310,
+  "./components/admin/collections/TaxonomyList#BlogCategoriesList": BlogCategoriesList_8f1d439cc289adf81730f9ebcc76c419,
+  "./components/admin/collections/TaxonomyList#BlogTagsList": BlogTagsList_8f1d439cc289adf81730f9ebcc76c419,
   "@payloadcms/richtext-lexical/client#BlocksFeatureClient": BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "./components/admin/collections/BlogList#default": default_61264ab934251a15e8ca2cc8f66bc206,
+  "./components/admin/collections/ContactInquiriesList#default": default_c23758562428c44820702092add4b596,
   "./components/admin/dashboard/CustomDashboard#default": default_f1a01e3ffbf6b019e40d01928db2823f,
   "./components/admin/views/AnalyticsView#default": default_9d80edae6c0796073a05eaf3a6882039
 }
